fec0d9cff645a9d59126bf8206a385a130c9bf42
mzakie <89344539+mzakie@users.noreply.github.com>
2021-08-22 14:14:27 +0100

    Add files via upload

---
+++ Convert.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using NBitcoin;
+
+namespace HiddenBitcoin.DataClasses
+{
+    internal static class Convert
+    {
+        internal static NBitcoin.Network ToNBitcoinNetwork(Network hNetwork)
+        {
+            if (hNetwork == Network.MainNet)
+                return NBitcoin.Network.Main;
+            if (hNetwork == Network.TestNet)
+                return NBitcoin.Network.TestNet;
+            throw new InvalidOperationException("WrongNetwork");
+        }
+
+        internal static Network ToHiddenBitcoinNetwork(NBitcoin.Network nNetwork)
+        {
+            if (nNetwork == NBitcoin.Network.Main)
+                return Network.MainNet;
+            if (nNetwork == NBitcoin.Network.TestNet)
+                return Network.TestNet;
+            throw new InvalidOperationException("WrongNetwork");
+        }
+
+        internal static ISecret ToISecret(string privateKey)
+        {
+            ISecret secret;
+
+            try
+            {
+                secret = new BitcoinSecret(privateKey);
+            }
+            catch
+            {
+                try
+                {
+                    secret = new BitcoinExtKey(privateKey);
+                }
+                catch
+                {
+                    throw new Exception($"Private key in wrong format: {privateKey}");
+                }
+            }
+
+            return secret;
+        }
+    }
+}
---
+++ FeeApi.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using NBitcoin;
+using Newtonsoft.Json.Linq;
+
+namespace HiddenBitcoin.DataClasses
+{
+    public enum FeeType
+    {
+        Fastest,
+        HalfHour,
+        Hour
+    }
+
+    internal static class FeesPerBytes
+    {
+        internal static DateTimeOffset LastUpdated;
+
+        internal static decimal Fastest;
+        internal static decimal HalfHour;
+        internal static decimal Hour;
+    }
+
+    public static class FeeApi
+    {
+        public static decimal GetRecommendedFee(int transactionSizeInBytes, FeeType feeType = FeeType.Fastest)
+        {
+            if (FeesPerBytes.LastUpdated == default(DateTimeOffset)
+                || DateTimeOffset.Now - FeesPerBytes.LastUpdated > TimeSpan.FromSeconds(7) )
+            {
+                UpdateFeesSync();
+            }
+
+            decimal feeInSatoshi;
+            switch (feeType)
+            {
+                case FeeType.Fastest:
+                    feeInSatoshi = FeesPerBytes.Fastest*transactionSizeInBytes;
+                    break;
+                case FeeType.HalfHour:
+                    feeInSatoshi = FeesPerBytes.HalfHour*transactionSizeInBytes;
+                    break;
+                case FeeType.Hour:
+                    feeInSatoshi = FeesPerBytes.Hour*transactionSizeInBytes;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(feeType), feeType, null);
+            }
+
+            var fee = new Money(feeInSatoshi, MoneyUnit.Satoshi);
+
+            return fee.ToDecimal(MoneyUnit.BTC);
+        }
+
+        private static void UpdateFeesSync()
+        {
+            UpdateFeesAsync().Wait();
+        }
+
+        // ReSharper disable once FunctionNeverReturns
+        private static async Task UpdateFeesAsync()
+        {
+            while (true)
+            {
+                using (var client = new HttpClient())
+                {
+                    const string request = @"https://bitcoinfees.21.co/api/v1/fees/recommended";
+
+                    var response = await client.GetAsync(request).ConfigureAwait(false);
+
+                    var result = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                    var json = JObject.Parse(result);
+                    FeesPerBytes.Fastest = json.Value<decimal>("fastestFee");
+                    FeesPerBytes.HalfHour = json.Value<decimal>("halfHourFee");
+                    FeesPerBytes.Hour = json.Value<decimal>("hourFee");
+                    FeesPerBytes.LastUpdated = DateTimeOffset.Now;
+                    return;
+                }
+            }
+        }
+    }
+}
